c7159ef0b5b1f7fc5e9b0fc62d5ee46c4ef7b389
Andreas Böttger <andreas.boettger@tuhh.de>
2019-02-14 10:23:55 +0000

    Update apache.sh

--- 5.0/apache.sh
+++ 5.0/apache.sh
@@ -6,8 +6,8 @@
 source /etc/apache2/envvars
 
 # logs should go to stdout / stderr
-set -ex \
-	&& ln -sfT /dev/stderr "$APACHE_LOG_DIR/error.log" \
-	&& ln -sfT /dev/stdout "$APACHE_LOG_DIR/access.log"
+set -eux; \
+	ln -sfT /dev/stderr "$APACHE_LOG_DIR/error.log"; \
+	ln -sfT /dev/stdout "$APACHE_LOG_DIR/access.log"
 
 apache2 -D FOREGROUND
